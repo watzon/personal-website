9470e50db37cf4ad6d08cbbced510ff5f2444937
Chris Watson <cawatson1993@gmail.com>
2024-08-16 10:26:08 -0600

    remove attempt at netlify identity script

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -52,15 +52,3 @@
         </footer>
     </div>
 </div>
-
-<script>
-    if (typeof window !== 'undefined' && window.netlifyIdentity) {
-      window.netlifyIdentity.on("init", user => {
-        if (!user) {
-          window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
-          });
-        }
-      });
-    }
-</script>
